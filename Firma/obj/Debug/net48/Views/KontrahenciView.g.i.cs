56bd556331b82a82fbc1ade17074c16ee5c4e622
Jakub Jarzmik <jakubjarzmik00@gmail.com>
2023-01-28 12:07:18 +0100

    Change background color of upper buttons in tabs

--- Firma/obj/Debug/net48/Views/KontrahenciView.g.i.cs
+++ Firma/obj/Debug/net48/Views/KontrahenciView.g.i.cs
@@ -53,7 +53,7 @@ namespace Firma.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Firma;V1.0.0.0;component/views/kontrahenciview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Firma;component/views/kontrahenciview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\KontrahenciView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
